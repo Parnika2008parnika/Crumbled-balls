a3052d81198bca3dbfb85c485df9941155748b2f
Parnika2008parnika <83539365+Parnika2008parnika@users.noreply.github.com>
2021-05-09 11:26:37 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,43 @@
+class Paper
+{
+	constructor(x,y,r)
+	{
+        var options={
+        isStatic:false,
+        restitution:0.3,
+        friction:0,
+        density:1.2
+        }
+		this.x=x;
+        this.y=y;
+        this.r=r;
+		//this.dustbinWidth=200;
+		//this.dustbinHeight=213;
+		//this.wallThickness=20;
+		
+		this.image=loadImage("paper.png")
+        this.body=Bodies.circle(this.x,this.y,(this.r-20)/2,options)
+        World.add(world,this.body);
+	}
+	display()
+	{
+			var paperpos=this.body.position;
+
+			
+
+			push()
+			translate(paperpos.x,paperpos.y);
+			rectMode(CENTER)
+			//strokeWeight(4);
+			//angleMode(RADIANS)
+			fill(255,0,255)
+			//rotate(this.angle)
+            //rect(0,0,this.wallThickness, this.dustbinHeight);
+             imageMode(CENTER);
+             image(this.image,0,0,this.r,this.r);
+			pop()
+
+			
+	}
+
+}
